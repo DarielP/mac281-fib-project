9e84a96fea9b0a8fd4c3d24c9678de3693e95e32
DarielP <dpantaleon0393@gmail.com>
2017-05-27 22:53:30 -0400

    Update fibLoops.cpp

--- fibLoops.cpp
+++ fibLoops.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-     long double n1 = 0,n2=1,next=0;
+     long double n1 = 0,n2=1,next=1;
 	int n;
 
     cout << "Enter a positive number: "<< endl;
     cin >> n;
     clock_t start, end;
     start = clock();
-	for (int i = 3; i <= n; ++i)
+	for (int i = 0; i <= n; ++i)
 	{
 		next = n1 + n2;
 		n1 = n2;
