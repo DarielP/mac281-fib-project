f87d8034a2b5e00fd61a9947c1e2396e4b8a9197
DarielP <dpantaleon0393@gmail.com>
2017-05-30 17:22:41 -0400

    Update primeFib.cpp
    Finds the first 10 prime numbers in the fibonacci series. PC is unable to find the next prime numbers because the process to find the next prime fibonacci number is repeated too many times since the next number is too large.

--- primeFib.cpp
+++ primeFib.cpp
@@ -1,63 +1,43 @@
-#include <iostream>
-#include <math.h>
-#include <ctime>
-#define MAX 1000
+#include<iostream> 
+#include<ctime>
+#include "InfInt.h"//allows big integers
 using namespace std;
-bool isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-    if (n == 2 || n == 3)
-    return true;
-    int checkRange = sqrt(n);
-    for (int i = 2; i <= checkRange; i++)
-    {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
+bool isPrime(InfInt); 
+int Fibonacci(int);
 int main(){
+int n = 0, counter = 0; 
+InfInt fib;
+clock_t start = clock(); 
+while(counter < 10)
 {
-	clock_t start;
-	double duration;
-	start= clock();
-    int n, c, first = 0, second = 1, next, primeCount = 0;
-    int primes[MAX];
+	fib = Fibonacci(n); 
+	if(isPrime(fib))
+	{
+		counter++; 
+	}
+		n++; 
+	}
 
-    cout << "Enter a positive number  " << endl;
-    cin >> n;
-
-    cout << "First " << n << " terms of Fibonacci series are :- " << endl;
-
-    for (c = 0; c < n; c++)
-    {
-        if (c <= 1)
-            next = c;
-        else
-        {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-
-        cout << next << endl;
-
-        if (isPrime(next))
-        {
-            primes[primeCount] = next;
-            primeCount++;
-        }
-    }
-
-    for (c = 0; c < primeCount; c++)
-        cout << primes[c] << ",";
-
-    duration=(clock()-start)/(double) CLOCKS_PER_SEC;
-    cout<<"Time taken: "<<duration<<"\n";
+clock_t end = clock();
+double execution = (double)(end-start)/CLOCKS_PER_SEC;
+cout << "execution time at " << counter << " prime Fibonacci numbers: " << execution << " seconds." << endl;
+return 0; 
+}
+bool isPrime(InfInt num){
+	if((num == 2) || (num == 0))
+	return false;
+for(InfInt i = "2"; i < num; i++)
+	{ 
+	if(num%i == 0)
+	}
+	return true; 
+	}
+int fibonacci(int n)
+{
+    if(n==0)
     return 0;
-
-}
-return 0;
-}
-
+    if(n==1)
+     return 1;
+    {
+        return(fibonacci(n-1)+fibonacci(n-2));
+    }
